6e49cb35213fbd405546336dba963dfc87f1a6f2
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 14:51:50 -0300

    Create media_notas_2alunos.js

---
+++ 06 - Problemas Registros/media_notas_2alunos.js
@@ -0,0 +1,29 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+function Aluno (nome, matricula, disciplina, nota) {
+    this.nome       = nome;
+    this.matricula  = matricula;
+    this.disciplina = disciplina;
+    this.nota       = nota;
+}
+
+function registrarAluno () {
+    var aluno = new Aluno ();
+    aluno.nome = input();
+    aluno.matricula = Number (input());
+    aluno.disciplina = input();
+    aluno.nota = Number (input());
+    
+    return aluno;
+}
+
+function media (aluno1, aluno2) {
+    var media = (aluno1.nota + aluno2.nota) / 2;
+    return media;
+}
+
+var alunox = registrarAluno();
+var alunoy = registrarAluno();
+
+console.log("Média = " + media (alunox, alunoy).toFixed(2));

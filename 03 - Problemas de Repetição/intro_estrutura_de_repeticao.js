2a7b04cafb2857374f5e9fe2678438101e2bfc3d
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 13:24:56 -0300

    Create intro_estrutura_de_repeticao.js

---
+++ 03 - Problemas de Repetição/intro_estrutura_de_repeticao.js
@@ -0,0 +1,10 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var quant = Number (input());
+var contador = 1;
+
+while (contador <= quant) {
+    console.log(contador + ") Vou estudar FUP todos os dias!");
+    contador ++;
+}

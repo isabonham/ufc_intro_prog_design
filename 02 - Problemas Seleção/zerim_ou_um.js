a3569153d538542f0240172bcce0867ba24d4539
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 13:06:13 -0300

    Create zerim_ou_um.js

---
+++ 02 - Problemas Seleção/zerim_ou_um.js
@@ -0,0 +1,19 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var jog1 = Number (input());
+var jog2 = Number (input());
+var jog3 = Number (input());
+
+if (jog1 === jog2 && (jog1 === jog3)) {
+    console.log ("empate");
+}
+else if ((jog2 === jog3) && (jog2 !== jog1)) {
+    console.log ("jog1");
+}
+else if ((jog1 === jog3) && (jog1 !== jog2)) {
+    console.log ("jog2");
+}
+else if ((jog1 === jog2) && (jog1 !== jog3)) {
+    console.log ("jog3");
+}

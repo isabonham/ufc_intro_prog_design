186488aba98be88b719ca460291dae373fc50ef4
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 14:30:28 -0300

    Create batida_policial.js

---
+++ 05 - Problemas Vetores/batida_policial.js
@@ -0,0 +1,27 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+function ordenar (vetor) {
+    for (var i = 0; i < vetor.length - 1; i++) {
+        for (var j = 0; j < (vetor.length - i - 1); j++) {
+            if (vetor[j] > vetor [j+1]) {
+                var aux = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = aux;
+            }
+        }
+    }
+    return vetor;
+}
+
+var tam = Number (input());
+var linha = input()
+var vetor = linha.split(" ").map(Number);
+
+if (linha[linha.length-1] === " ") {
+    vetor.pop();
+}
+
+ordenar(vetor);
+
+console.log(vetor);

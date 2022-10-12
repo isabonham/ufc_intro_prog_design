70edb95c833f9a4c4f1dce555bd23fa69976881b
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 12:10:44 -0300

    Create casa_triangular.js

---
+++ 01 - Problemas Estrutura Sequencial/casa_triangular.js
@@ -0,0 +1,15 @@
+// interação com o teclado
+// habilitar o input a partir do teclado
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift;
+
+
+var lado1 = Number (input());
+var lado2 = Number (input());
+var lado3 = Number (input());
+
+var p = (lado1 + lado2 + lado3) / 2;
+
+var area = Math.sqrt(p*(p-lado1)*(p-lado2)*(p-lado3));
+
+console.log(area.toFixed(2));

c3e4aa99f90ffa5b9d2a06efcbdedd8818dc25d1
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 14:29:19 -0300

    Create capoeira.js

---
+++ 05 - Problemas Vetores/capoeira.js
@@ -0,0 +1,15 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var quant = Number (input());
+
+var vetor = input();
+vetor = vetor.split(" ");
+
+var vetori = [];
+
+for (var i = vetor.length - 1; i >= 0; i--) {
+    vetori.push(vetor[i]);
+}
+
+console.log(vetori);

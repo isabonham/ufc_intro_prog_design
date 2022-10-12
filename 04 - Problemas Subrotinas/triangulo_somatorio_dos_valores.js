252646b9ceaf9441a7ae7c328d7579360652a4e2
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 13:51:09 -0300

    Create triangulo_somatorio_dos_valores.js

---
+++ 04 - Problemas Subrotinas/triangulo_somatorio_dos_valores.js
@@ -0,0 +1,33 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+function somatorio (num) {
+    var soma = 0;
+    
+    for (var a = num; a >= 1; a--) {
+        soma += a;
+    }
+    
+    return soma;
+}
+
+function triangulo (num) {
+    var soma = 0;
+    
+    for (var a = 1; a <= num; a++) {
+        var imprimir = "";
+        var x = 1;
+        while (x <= a) {
+            imprimir += x+" "
+            x++;
+        }
+        soma += somatorio (a);
+        console.log (imprimir);
+    }
+    
+    console.log(soma);
+}
+
+var num = Number (input());
+
+triangulo (num);

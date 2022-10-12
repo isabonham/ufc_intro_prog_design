4177fb0be2e0d0c71f1b1143b2d5776304176eb8
Isabela Vieira <isa.bonham@gmail.com>
2022-10-12 13:26:29 -0300

    Create somatório_v1.js

---
+++ 03 - Problemas de Repetição/somatório_v1.js
@@ -0,0 +1,14 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var num1 = Number (input());
+var num2 = Number (input());
+
+var soma = 0;
+
+while (num1 <= num2){
+    soma +=num1;
+    num1 ++;
+}
+
+console.log (soma);

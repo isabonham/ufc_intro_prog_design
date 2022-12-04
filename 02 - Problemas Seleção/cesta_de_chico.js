fabe9d91e8d8b32ebf0827ca15918cdd6f974312
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 11:02:33 -0300

    Create cesta_de_chico.js

---
+++ 02 - Problemas Seleção/cesta_de_chico.js
@@ -0,0 +1,23 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var cesta = Number(input());
+var b = Number(input());
+var g = Number(input());
+var m = Number(input());
+
+var frutas = b + g + m;
+
+var tempo = 1;
+
+if(cesta >= frutas) {
+    tempo = tempo;
+}
+
+else {
+    while(cesta < frutas) {
+        tempo += 1;
+        frutas -= cesta;
+    }
+}
+console.log(tempo);

151f9f042d9f7424642b189a86d0cad70dd3e128
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 12:48:21 -0300

    Create soma_maior_menor.js

---
+++ 03 - Problemas de Repetição/soma_maior_menor.js
@@ -0,0 +1,22 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var maior = 0;
+var menor = 200;
+
+var x = 0;
+
+while(x < 5) {
+    var num = Number(input());
+    if(num < menor) {
+        menor = num;
+    }
+    if(num > maior) {
+        maior = num;
+    }
+    x++;
+}
+
+var soma = menor + maior;
+
+console.log(soma);

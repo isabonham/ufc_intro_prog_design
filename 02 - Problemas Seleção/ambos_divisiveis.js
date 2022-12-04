454db0dc419dc5d842f63232ab6085f9a4ea7870
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 11:02:22 -0300

    Create ambos_divisiveis.js

---
+++ 02 - Problemas Seleção/ambos_divisiveis.js
@@ -0,0 +1,12 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var num1 = Number(input());
+var num2 = Number(input());
+
+if((num1 % 3 === 0 && num2 % 3 === 0) || (num1 % 5 === 0 && num2 % 5 === 0)) {
+    console.log("sim");
+}
+else {
+    console.log("nao");
+}

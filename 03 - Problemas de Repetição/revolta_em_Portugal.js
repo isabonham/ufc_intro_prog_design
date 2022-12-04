cd9de2fa1c396a8546118cb7a13222199767f293
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 12:48:27 -0300

    Create revolta_em_Portugal.js

---
+++ 03 - Problemas de Repetição/revolta_em_Portugal.js
@@ -0,0 +1,28 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var quant = Number(input());
+
+var soldados = 0;
+var rebeldes = 0;
+
+while(quant > 0) {
+    var combatente = Number(input());
+    if(combatente%2 === 0) {
+        rebeldes += combatente;
+    }
+    else {
+        soldados += combatente;
+    }
+    quant--;
+}
+
+if(rebeldes > soldados) {
+    console.log("rebeldes");
+}
+else if(soldados > rebeldes) {
+    console.log("soldados");
+}
+else {
+    console.log("empate");
+}

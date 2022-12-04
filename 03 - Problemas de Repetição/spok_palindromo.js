597539f3d6fb8ec9facd2d57519a972dc460ac8c
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 12:48:01 -0300

    Create spok_palindromo.js

---
+++ 03 - Problemas de Repetição/spok_palindromo.js
@@ -0,0 +1,19 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var num = Number(input());
+var num2 = 0;
+
+var x = num;
+
+while(x > 0) {
+    num2 = (num2*10) + (x%10);
+    x = Math.floor(x/10);
+}
+
+if(num === num2) {
+    console.log("1");
+}
+else {
+    console.log("0");
+}

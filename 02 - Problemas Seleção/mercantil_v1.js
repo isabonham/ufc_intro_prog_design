b56835d8bf2359c31b61f70869209df52e80c042
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 11:02:53 -0300

    Create mercantil_v1.js

---
+++ 02 - Problemas Seleção/mercantil_v1.js
@@ -0,0 +1,19 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var produto = Number(input());
+var jog1 = Number(input());
+var jog2 = Number(input());
+
+var result1 = Math.abs(produto - jog1);
+var result2 = Math.abs(produto - jog2);
+
+if(result1 === result2) {
+    console.log("empate");
+}
+else if(result1 < result2) {
+    console.log("primeiro");
+}
+else if(result1 > result2) {
+    console.log("segundo");
+}

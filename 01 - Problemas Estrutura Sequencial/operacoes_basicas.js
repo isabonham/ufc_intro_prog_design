5cef551ea698b9ae7507662e0b0e78e1c519054e
Isabela Vieira <isa.bonham@gmail.com>
2022-12-04 09:45:45 -0300

    Create operacoes_basicas.js

---
+++ 01 - Problemas Estrutura Sequencial/operacoes_basicas.js
@@ -0,0 +1,19 @@
+// habilitar o input a partir do teclado
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+
+var numero1 = Number(input());
+var numero2 = Number(input());
+ 
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1 / numero2;
+var resto = numero1 % numero2;
+ 
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao.toFixed(2));
+console.log(resto);

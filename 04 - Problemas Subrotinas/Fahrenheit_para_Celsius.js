b8b9f9cd0516ecc05887125e9fb41fada3454da4
Isabela Vieira <isa.bonham@gmail.com>
2022-12-19 20:54:36 -0300

    Create Fahrenheit_para_Celsius.js

---
+++ 04 - Problemas Subrotinas/Fahrenheit_para_Celsius.js
@@ -0,0 +1,11 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var F = Number(input());
+
+function converte_para_celsius(temperatura) {
+    var C = (temperatura - 32) * 5/9;
+    return C;
+}
+
+console.log(converte_para_celsius(F).toFixed(6));

8aa68399b825cb1d32de3a70455f2f3a785fd437
Isabela Vieira <isa.bonham@gmail.com>
2022-12-29 14:19:10 -0300

    Create parkour.js

---
+++ 05 - Problemas Vetores/parkour.js
@@ -0,0 +1,21 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var quant = Number(input());
+
+var vetor = input();
+vetor = vetor.split(" ");
+
+for(var i = 0; i < vetor.length; i++) {
+    vetor[i] = Number(vetor[i]);
+}
+
+var pulos = 0;
+
+for(var i = 1; i < vetor.length; i++) {
+    if((Math.abs(vetor[i] - vetor[i-1])) > 1) {
+        pulos++;
+    }
+}
+
+console.log(pulos);

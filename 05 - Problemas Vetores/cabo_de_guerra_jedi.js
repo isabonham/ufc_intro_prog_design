4db436f8ad75f3b1c809f9406cf62cc26566ffe9
Isabela Vieira <isa.bonham@gmail.com>
2022-12-29 14:18:21 -0300

    Create cabo_de_guerra_jedi.js

---
+++ 05 - Problemas Vetores/cabo_de_guerra_jedi.js
@@ -0,0 +1,32 @@
+var lines = require("fs").readFileSync(0).toString().split("\n");
+var input = () => lines.shift();
+
+var quant = Number(input());
+var somaj = 0;
+var somas = 0;
+vetor = input();
+vetor = vetor.split(" ");
+
+for(var i = 0; i < vetor.length; i++) {
+    vetor[i] = Number(vetor[i]);
+}
+
+var metade = vetor.length / 2;
+
+for(var i = 0; i < metade; i++) {
+    somaj += vetor [i];
+}
+
+for(var i = metade; i < vetor.length; i++) {
+    somas += vetor [i];
+}
+
+if(somaj > somas) {
+    console.log ("Jedi");
+}
+else if(somas > somaj) {
+    console.log ("Sith");
+}
+else {
+    console.log ("empate");
+}
